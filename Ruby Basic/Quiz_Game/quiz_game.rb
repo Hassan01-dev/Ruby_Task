0577c51ec8ff76e9df19b9636a74e4352cc3b23c
Hassan Raza <88370398+Hassan-Devsinc@users.noreply.github.com>
2021-08-12 19:20:18 +0500

    Ruby Task

---
+++ Ruby Basic/Quiz_Game/quiz_game.rb
@@ -0,0 +1,7 @@
+require 'csv'
+load'quiz_class.rb'     #Loading the quiz class
+load'main_class.rb'     #Loading the main class
+
+#Creating the object of Main Class to start program
+x = Main_menu.new
+x.menu
---
+++ Ruby Basic/Weather_man/weatherman.rb
@@ -0,0 +1,8 @@
+require 'colorize'
+require 'date'
+
+load'helper_module.rb'
+load'main_class.rb'
+
+x = Main.new
+x.start
